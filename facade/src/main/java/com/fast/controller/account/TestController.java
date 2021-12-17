8438bbdc8131b7b84fd3d7bd86f7f4f892ea8342
huapeiliang <2473095423@qq.com>
2021-12-17 17:24:39 +0800

    分页插件

--- facade/src/main/java/com/fast/controller/account/TestController.java
+++ facade/src/main/java/com/fast/controller/account/TestController.java
@@ -2,8 +2,6 @@ package com.fast.controller.account;
 
 import com.fast.api.account.AccountApi;
 import com.fast.configuration.BaseResponse;
-import com.fast.model.FastRunTimeException;
-import com.fast.model.ReturnData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +18,6 @@ public class TestController {
     @PostMapping("/throw-fast-exception")
     public void throwFastException(){
         accountApi.throwFastException();
-    }
-
-    @PostMapping("/throw-fast-exception2")
-    public void throwFastException2(){
-        int i=0/0;
     }
 
 }
